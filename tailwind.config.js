1932b1e6498c1a4d9882f73513b1134c997f2bfa
Jared Sy <jaredblase@gmail.com>
2024-08-02 13:50:58 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+		"./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+	],
+  theme: {
+		container: {
+			center: true,
+		},
+    extend: {
+			colors: {
+				primary: "#040e18",
+			}
+		},
+  },
+  plugins: [],
+}
+
