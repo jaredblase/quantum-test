af8292f9885326948f615e8a784b74449fc062d4
Jared Sy <jaredblase@gmail.com>
2024-08-04 16:09:14 +0800

    add (component): header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,68 @@
+import { Link, useLocation } from 'react-router-dom'
+import { cn } from '@/lib/utils'
+import { MenuIcon } from '../icons/menu'
+import { useState } from 'react'
+
+const links = [
+	{ href: '/', title: 'Quantum Pulse Consulting' },
+	{ href: '/about-us', title: 'About Us' },
+	{ href: '/experiences', title: 'Experiences' },
+	{ href: '/case-studies', title: 'Case Studies' },
+	{ href: '/contact-us', title: 'Contact Us' },
+]
+
+export function Header() {
+	const location = useLocation()
+	const [isMenuOpen, setMenuOpen] = useState(false)
+
+	return (
+		<header className="relative bg-[#040E19] text-white">
+			<div className="container flex h-20 items-center justify-between">
+				<Link to="/">
+					<img src="/logo.webp" alt="Quantum Pulse Logo" className="w-36" />
+				</Link>
+				<nav className="hidden gap-8 lg:flex">
+					{links.map((l) => (
+						<Link
+							key={l.href}
+							to={l.href}
+							className={cn(
+								'link decoration-white underline-offset-8',
+								location.pathname === l.href && 'text-[#597EB5] underline'
+							)}
+						>
+							{l.title}
+						</Link>
+					))}
+				</nav>
+				{/* Mobile Menu */}
+				<MenuIcon
+					className="lg:hidden"
+					onClick={() => setMenuOpen((x) => !x)}
+					open={isMenuOpen}
+				/>
+				<div
+					className={cn(
+						'pointer-events-none absolute left-0 top-full w-full -translate-y-4 gap-8 bg-primary opacity-0 transition-all duration-300 lg:hidden',
+						isMenuOpen && 'pointer-events-auto block translate-y-0 opacity-100'
+					)}
+				>
+					<nav className="container flex flex-col gap-4 py-4">
+						{links.map((l) => (
+							<Link
+								key={l.href}
+								to={l.href}
+								className={cn(
+									'link decoration-white underline-offset-8',
+									location.pathname === l.href && 'text-[#597EB5] underline'
+								)}
+							>
+								{l.title}
+							</Link>
+						))}
+					</nav>
+				</div>
+			</div>
+		</header>
+	)
+}
