c17004107c7b915c8a65321b271a24c2c7cde0cb
Jared Sy <jaredblase@gmail.com>
2024-08-05 17:03:09 +0800

    update: setup font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-		"./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-	],
-  theme: {
-		container: {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		container: (theme) => ({
 			center: true,
-		},
-    extend: {
+			padding: theme('spacing.8'),
+		}),
+		extend: {
 			colors: {
-				primary: "#040e18",
-			}
+				primary: '#040e19',
+			},
+			fontFamily: {
+				sans: ['Acumin-SemiCondensed', ...fontFamily.sans],
+			},
 		},
-  },
-  plugins: [],
+	},
+	plugins: [],
 }
-
